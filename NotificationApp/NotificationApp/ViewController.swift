bc6749d56479efa44e4284ff80f2ee44fcf983ec
Nick Ingels <nick.ingels@student.ehb.be>
2021-11-06 13:54:17 +0100

    created project

---
+++ NotificationApp/NotificationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotificationApp
+//
+//  Created by Nick Ingels on 06/11/2021.
+//  Copyright © 2021 EHB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
