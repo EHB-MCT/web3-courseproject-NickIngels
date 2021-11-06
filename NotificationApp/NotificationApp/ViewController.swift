a63b7836377a3d1751394babcea755be9dd19595
Nick Ingels <nick.ingels@student.ehb.be>
2021-11-06 16:19:39 +0100

    Notificaties werken

--- NotificationApp/NotificationApp/ViewController.swift
+++ NotificationApp/NotificationApp/ViewController.swift
@@ -10,17 +10,33 @@ import UIKit
 import UserNotifications
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let center = UNUserNotificationCenter.current()
+        let notificatie = UNUserNotificationCenter.current()
         
-        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+        notificatie.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
             
         }
         
+        let inhoud = UNMutableNotificationContent()
+        inhoud.title = "Dit is een notificatietest"
+        inhoud.body = "Ik ben je notificatie"
         
+        let datum = Date().addingTimeInterval(10)
+        let datumComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: datum)
+         
+        let trigger = UNCalendarNotificationTrigger(dateMatching: datumComponents, repeats: false)
+        
+        let uuid = UUID().uuidString
+        
+        let request = UNNotificationRequest(identifier: uuid, content: inhoud, trigger: trigger)
+        
+        notificatie.add(request) { (error) in
+            
+        }
     }
 
 
